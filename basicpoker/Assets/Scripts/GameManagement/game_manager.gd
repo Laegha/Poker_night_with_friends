3b7ca8446a2c7913ea48eb0f8da7902477ba5e9c
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2024-12-30 03:31:36 -0300

    feat(game_manager): variable storing the first not global node in hierarchy

--- basicpoker/Assets/Scripts/GameManagement/game_manager.gd
+++ basicpoker/Assets/Scripts/GameManagement/game_manager.gd
@@ -3,5 +3,16 @@ extends Node
 var player_name: String
 var scene_manager: SceneManager
 
+var root_node: Node
+
+func _ready() -> void:
+	for child in get_tree().root.get_children():
+		if not child.is_class("AutoLoad"):
+			root_node = child
+
 func update_player_name(new_name: String):
 	player_name = new_name
+
+func show_full_tree():
+	print("Full tree:")
+	print(scene_manager.get_tree_string_pretty())
