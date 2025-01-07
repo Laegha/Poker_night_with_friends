fd6361c6014d0083422ff9aa164cbc9eed84d168
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-07 15:18:21 -0300

    feat(multiplayer_manager): udp_socket setup for both host and client

--- basicpoker/Assets/Scripts/OnlineManagement/multiplayer_manager.gd
+++ basicpoker/Assets/Scripts/OnlineManagement/multiplayer_manager.gd
@@ -2,6 +2,15 @@ extends Node
 
 var host: Host
 
+var udp_socket: PacketPeerUDP
+var BRODCAST_PORT: int = 4242
+
+func _ready() -> void:
+	udp_socket = PacketPeerUDP.new()
+	udp_socket.set_broadcast_enabled(true)
+	udp_socket.set_dest_address("255.255.255.255", BRODCAST_PORT)
+	udp_socket.bind(BRODCAST_PORT)
+	
 func set_host():
 	#rpc_id(1, "get_host", self)
 	print("Client trying to get host")
