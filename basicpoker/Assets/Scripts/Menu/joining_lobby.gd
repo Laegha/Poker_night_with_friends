e66903a9c771bda06178f26ab2a8c48719f344d7
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-07 15:18:54 -0300

    feat(joining_lobby): test recieving packets

--- basicpoker/Assets/Scripts/Menu/joining_lobby.gd
+++ basicpoker/Assets/Scripts/Menu/joining_lobby.gd
@@ -3,7 +3,13 @@ extends Control
 var main_menu_scene: String = "res://Scenes/MainMenu/mainmenu.tscn"
 
 func _ready() -> void:
-	print("MainMenu from joiningLobby: " + str(main_menu_scene))
-
+	print("PACKET RECIEVED: " + str(MultiplayerManager.udp_socket.get_packet()))
 func _on_return_btn_pressed() -> void:
 	GameManager.scene_manager.load_gui_scene(main_menu_scene)
+
+func generate_lobby_label(lobby_name: String):
+	pass
+	
+func update_lobby_members(lobby_label: LobbyLabel):
+	PacketPeer
+	pass
