72841255f5946b5dbf33be23f157ccf0f7cb3c24
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-07 15:20:05 -0300

    feat(lobby_host): handling packet sending

--- basicpoker/Assets/Scripts/Menu/lobby_host.gd
+++ basicpoker/Assets/Scripts/Menu/lobby_host.gd
@@ -12,22 +12,19 @@ func get_packet_data() -> Dictionary:
 		"GAMEMODE" : MultiplayerManager.host.game_mode,
 	}
 
-var udp_socket: PacketPeerUDP
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#MultiplayerManager.host.
-	udp_socket = PacketPeerUDP.new()
-	udp_socket.set_broadcast_enabled(true)
-	
 	MultiplayerManager.host.player_added.connect(add_player_to_list)
 	MultiplayerManager.host.player_removed.connect(remove_player_from_list)
 	
-	udp_socket.set_dest_address("255.255.255.255", BRODCAST_PORT)
 	brodcast_packet()
 
 func brodcast_packet():
 	var packet: String = MultiplayerManager.get_packet(get_packet_data())
-	udp_socket.put_packet(packet.to_utf8_buffer())
+	var buffer_packet: PackedByteArray = packet.to_utf8_buffer()
+	var error = MultiplayerManager.udp_socket.put_packet(buffer_packet)
+	print("Available packets" + str(MultiplayerManager.udp_socket.get_available_packet_count()))
+	print("Sent packet2" + str(buffer_packet))
 
 func add_player_to_list(id: int):
 	print("Host detected the join of " + str(id))
