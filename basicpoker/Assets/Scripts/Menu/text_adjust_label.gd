78d4ec2edc487629dc5385d01441cb98e7f4a9a4
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-07 14:16:12 -0300

    feat(text_adjust_label): label with text that shrinks if it doesn't fit

---
+++ basicpoker/Assets/Scripts/Menu/text_adjust_label.gd
@@ -0,0 +1,21 @@
+extends Label
+
+@export var min_text_to_fill_scale: float
+@export var min_font_size: int
+func text_changed():
+	#print(is_text_clipped())
+	var font_size =  get_theme_font_size("font_size")
+	print(font_size)
+	while is_text_clipped(font_size):
+		font_size -= 16
+		add_theme_font_size_override("font_size", font_size)
+	print(font_size)
+	
+func _ready() -> void:
+	text_changed()
+
+func is_text_clipped(font_size) -> bool:
+	var label_width = size.x
+	var text_width = get_theme_font("font").get_string_size(text).x * min_text_to_fill_scale * font_size / min_font_size
+	print(text_width)
+	return text_width > label_width
