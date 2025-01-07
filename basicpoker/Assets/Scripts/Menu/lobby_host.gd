9608c8c59fa9b567faa1de2d25510cd7abbe98ce
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-07 14:44:37 -0300

    feat(lobby_host): setting packet address and sending packets
    Also fixed packet_data, made into a function so it updates when connected_players_ids changes

--- basicpoker/Assets/Scripts/Menu/lobby_host.gd
+++ basicpoker/Assets/Scripts/Menu/lobby_host.gd
@@ -4,12 +4,13 @@ extends Control
 var player_label_scene: String = ""
 var BRODCAST_PORT: int = 4242
 
-var packet_data: Dictionary = {
-	"IP" : MultiplayerManager.host.SERVER_IP,
-	"PORT" : MultiplayerManager.host.SERVER_PORT,
-	"PLAYERS" : MultiplayerManager.host.connected_players_ids.count(),
-	"GAMEMODE" : MultiplayerManager.host.game_mode,
-}
+func get_packet_data() -> Dictionary:
+	return{
+		"IP" : MultiplayerManager.host.SERVER_IP,
+		"PORT" : MultiplayerManager.host.SERVER_PORT,
+		"PLAYERS" : MultiplayerManager.host.connected_players_ids.size(),
+		"GAMEMODE" : MultiplayerManager.host.game_mode,
+	}
 
 var udp_socket: PacketPeerUDP
 # Called when the node enters the scene tree for the first time.
@@ -21,13 +22,14 @@ func _ready() -> void:
 	MultiplayerManager.host.player_added.connect(add_player_to_list)
 	MultiplayerManager.host.player_removed.connect(remove_player_from_list)
 	
-	
+	udp_socket.set_dest_address("255.255.255.255", BRODCAST_PORT)
 	brodcast_data()
-	
+
+
+
 func brodcast_data():
-	var packet: String
-	udp_socket.put_packet()
-	pass
+	var packet: String = MultiplayerManager.get_packet(get_packet_data())
+	udp_socket.put_packet(packet.to_utf8_buffer())
 
 func add_player_to_list(id: int):
 	print("Host detected the join of " + str(id))
