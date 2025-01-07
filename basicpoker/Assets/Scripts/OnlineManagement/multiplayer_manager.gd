8fd5bcc671762c5c7342b6edb6f713e222612257
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-07 14:13:13 -0300

    feat(multiplayer_manager): rpc testing functions

--- basicpoker/Assets/Scripts/OnlineManagement/multiplayer_manager.gd
+++ basicpoker/Assets/Scripts/OnlineManagement/multiplayer_manager.gd
@@ -1,3 +1,22 @@
 extends Node
 
 var host: Host
+
+func set_host():
+	#rpc_id(1, "get_host", self)
+	print("Client trying to get host")
+	var error = rpc_id(1, "test_rpc")
+	print(str(error) + " While testing rpc")
+	print(error)
+
+func get_packet(dict: Dictionary) -> String:
+	pass
+
+@rpc("any_peer", "call_remote", "unreliable")
+func test_rpc():
+	print("Testing rpc on " + str(multiplayer.get_unique_id()))
+
+@rpc("any_peer")
+func get_host(multiplayer_manager: MultiplayerManager):
+	print("Called get_host on id: " + str(multiplayer.get_unique_id()))
+	multiplayer_manager.host = host
