cdde705553958b43d18a3fb74b0528c00c578727
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-09 13:21:15 -0300

    feat(joining_lobby): wait until a packet is recieved before trying to get it

--- basicpoker/Assets/Scripts/Menu/joining_lobby.gd
+++ basicpoker/Assets/Scripts/Menu/joining_lobby.gd
@@ -3,6 +3,8 @@ extends Control
 var main_menu_scene: String = "res://Scenes/MainMenu/mainmenu.tscn"
 
 func _ready() -> void:
+	while MultiplayerManager.udp_socket.wait() != OK:
+		await get_tree().process_frame
 	print("PACKET RECIEVED: " + str(MultiplayerManager.udp_socket.get_packet()))
 func _on_return_btn_pressed() -> void:
 	GameManager.scene_manager.load_gui_scene(main_menu_scene)
