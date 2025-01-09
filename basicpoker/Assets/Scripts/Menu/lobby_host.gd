035e68e24956d7d875f544981a8b656370558e01
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-09 13:35:16 -0300

    refactor(lobby_host): get_packet_data moved to lobby

--- basicpoker/Assets/Scripts/Menu/lobby_host.gd
+++ basicpoker/Assets/Scripts/Menu/lobby_host.gd
@@ -4,14 +4,6 @@ extends Control
 var player_label_scene: String = ""
 var BRODCAST_PORT: int = 4242
 
-func get_packet_data() -> Dictionary:
-	return{
-		"IP" : MultiplayerManager.host.SERVER_IP,
-		"PORT" : MultiplayerManager.host.SERVER_PORT,
-		"PLAYERS" : MultiplayerManager.host.connected_players_ids.size(),
-		"GAMEMODE" : MultiplayerManager.host.game_mode,
-	}
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	MultiplayerManager.host.player_added.connect(add_player_to_list)
@@ -20,7 +12,7 @@ func _ready() -> void:
 	brodcast_packet()
 
 func brodcast_packet():
-	var packet: String = MultiplayerManager.get_packet(get_packet_data())
+	var packet: String = MultiplayerManager.get_packet_from_dict(MultiplayerManager.host.lobby.get_packet_data())
 	var buffer_packet: PackedByteArray = packet.to_utf8_buffer()
 	var error = MultiplayerManager.udp_socket.put_packet(buffer_packet)
 	print("Available packets" + str(MultiplayerManager.udp_socket.get_available_packet_count()))
