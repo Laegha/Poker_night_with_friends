815bf0cc6fccbdd995a150fc191217fa6388ab67
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-09 13:38:09 -0300

    feat(multiplayer_manager): packet conversion from dict to string and backwards

--- basicpoker/Assets/Scripts/OnlineManagement/multiplayer_manager.gd
+++ basicpoker/Assets/Scripts/OnlineManagement/multiplayer_manager.gd
@@ -5,6 +5,8 @@ var host: Host
 var udp_socket: PacketPeerUDP
 var BRODCAST_PORT: int = 4242
 
+var packet_item_separation = "/:"
+
 func _ready() -> void:
 	udp_socket = PacketPeerUDP.new()
 	udp_socket.set_broadcast_enabled(true)
@@ -18,11 +20,29 @@ func set_host():
 	print(str(error) + " While testing rpc")
 	print(error)
 
-func get_packet(dict: Dictionary) -> String:
+func get_packet_from_dict(dict: Dictionary) -> String:
 	var packet: String = ""
 	for key: String in dict:
-		packet += str(key) + ":" + str(dict[key]) + ":"
+		packet += str(key) + packet_item_separation + str(dict[key]) + packet_item_separation
 	return packet
+	
+func get_dict_from_packet(packet: String, model_dict: Dictionary):
+	var dict: Dictionary = {}
+	var keys = model_dict.keys
+	var separations_indexes: Array[int] = GameManager.find_all_occurrences(packet, packet_item_separation)
+	for key: String in keys:
+		var value_start_index: int = packet.find(key) + key.length() + packet_item_separation.length()
+		var value_end_index: int
+		var larger_separations_indexes: Array[int] = separations_indexes.filter(func(x: int):	x > value_start_index)
+		if larger_separations_indexes.is_empty():
+			value_end_index = packet.length() -1
+		else: 
+			value_end_index = larger_separations_indexes.min()
+		
+		var str_value: String = packet.substr(value_start_index, value_end_index - value_start_index)
+		
+		dict[key] = str_value
+	return dict
 
 @rpc("any_peer", "call_remote", "unreliable")
 func test_rpc():
