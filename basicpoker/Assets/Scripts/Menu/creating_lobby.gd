f14040c8c96b617f313ac2d0cbedf57fc7d6a182
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-09 13:14:25 -0300

    feat(creating_lobby): lobby name filtering
    This was meant to avoid confusing characters in the name with packet separations. Still have to do the unfilter

--- basicpoker/Assets/Scripts/Menu/creating_lobby.gd
+++ basicpoker/Assets/Scripts/Menu/creating_lobby.gd
@@ -10,14 +10,23 @@ var host_scene: String = "res://Scenes/Multiplayer/host.tscn"
 
 func _on_create_btn_pressed() -> void:
 	var host: Host = load(host_scene).instantiate()
-	MultiplayerManager.host = host
 	GameManager.root_node.add_child(host)
-	host.lobby_name = lobby_name_le.text
-	host.game_mode = game_modes_btn.text
-	host.initial_chips = int(initial_chips_nle.text)
+	host.lobby.lobby_name = lobby_name_le.text
+	host.lobby.filtered_lobby_name = filter_lobby_name(lobby_name_le.text)
+	host.lobby.game_mode = game_modes_btn.text
+	host.lobby.initial_chips = int(initial_chips_nle.text)
 	await host.start_host()
+	MultiplayerManager.host = host
 	GameManager.scene_manager.load_gui_scene(lobby_host_scene)
 	
+func filter_lobby_name(lobby_name: String):
+	var filtered_name: String = lobby_name
+	
+	var separations_in_name: Array[int] = GameManager.find_all_occurrences(lobby_name, MultiplayerManager.packet_item_separation)
+	for separation_index: int in separations_in_name:
+		filtered_name[separation_index] = Lobby.name_filter
+	
+	return filtered_name
 
 func _on_return_btn_pressed() -> void:
 	GameManager.scene_manager.load_gui_scene(main_menu_scene)
