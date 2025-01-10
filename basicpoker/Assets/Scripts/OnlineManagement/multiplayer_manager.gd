7ff4db6d513045622d6d9193b7928586b34892e0
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-10 17:08:33 -0300

    fix(multiplayer_manager): get_dict_from_packet separating keys properly
    The lambda didn't return the logic so it was assigning all the packet always

--- basicpoker/Assets/Scripts/OnlineManagement/multiplayer_manager.gd
+++ basicpoker/Assets/Scripts/OnlineManagement/multiplayer_manager.gd
@@ -3,7 +3,7 @@ extends Node
 var host: Host
 
 var udp_socket: PacketPeerUDP
-var BRODCAST_PORT: int = 4242
+var BRODCAST_PORT: int = 4343
 
 var packet_item_separation = "/:"
 
@@ -26,22 +26,24 @@ func get_packet_from_dict(dict: Dictionary) -> String:
 		packet += str(key) + packet_item_separation + str(dict[key]) + packet_item_separation
 	return packet
 	
-func get_dict_from_packet(packet: String, model_dict: Dictionary):
+func get_dict_from_packet(packet: String, keys: Array[String]):
 	var dict: Dictionary = {}
-	var keys = model_dict.keys
 	var separations_indexes: Array[int] = GameManager.find_all_occurrences(packet, packet_item_separation)
 	for key: String in keys:
 		var value_start_index: int = packet.find(key) + key.length() + packet_item_separation.length()
 		var value_end_index: int
-		var larger_separations_indexes: Array[int] = separations_indexes.filter(func(x: int):	x > value_start_index)
+		var larger_separations_indexes: Array[int] = separations_indexes.filter(func(x: int):	return x > value_start_index)
+		print("LARGER SEPORASDAWEOK IMNDEXES " + str(larger_separations_indexes))
+		
 		if larger_separations_indexes.is_empty():
-			value_end_index = packet.length() -1
+			value_end_index = packet.length()
 		else: 
 			value_end_index = larger_separations_indexes.min()
 		
 		var str_value: String = packet.substr(value_start_index, value_end_index - value_start_index)
 		
 		dict[key] = str_value
+	print("Obtained dictionary " + str(dict))
 	return dict
 
 @rpc("any_peer", "call_remote", "unreliable")
