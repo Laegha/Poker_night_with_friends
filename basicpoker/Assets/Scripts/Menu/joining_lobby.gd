acbe1c8a583ac123a4ec9ae4cf25952b67b7e94a
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-10 17:11:28 -0300

    feat(joining_lobby): recieving packets and creating labels
    This is still bugged, because sometimes it returns a non descriptive error when recieving packets

--- basicpoker/Assets/Scripts/Menu/joining_lobby.gd
+++ basicpoker/Assets/Scripts/Menu/joining_lobby.gd
@@ -2,16 +2,39 @@ extends Control
 
 var main_menu_scene: String = "res://Scenes/MainMenu/mainmenu.tscn"
 
-func _ready() -> void:
-	while MultiplayerManager.udp_socket.wait() != OK:
-		await get_tree().process_frame
-	print("PACKET RECIEVED: " + str(MultiplayerManager.udp_socket.get_packet()))
+var available_lobbys: Dictionary
+
+@export var lobby_label_container: BoxContainer
+var lobby_label_scene: String = "res://Scenes/MainMenu/lobby_label.tscn"
+
+func _process(delta: float) -> void:
+	var packet_count: int = MultiplayerManager.udp_socket.get_available_packet_count()
+	if packet_count <= 0:
+		print("ERROR RECIEVING PACKET, AVAILABLE PACKETS ARE " + str(packet_count))
+		return
+	print("AVAILABLE PACKETS: " + str(packet_count))
+	var recieved_packet: String = MultiplayerManager.udp_socket.get_packet().get_string_from_utf8()
+	print("PACKET RECIEVED: " + recieved_packet)
+	on_packet_recieved(recieved_packet)
+
 func _on_return_btn_pressed() -> void:
 	GameManager.scene_manager.load_gui_scene(main_menu_scene)
 
-func generate_lobby_label(lobby_name: String):
-	pass
+func on_packet_recieved(packet_data: String):
+	var data: Dictionary = MultiplayerManager.get_dict_from_packet(packet_data, LobbyData.packet_keys)
+	var new_lobby_data: LobbyData = LobbyData.new()
+	new_lobby_data.set_packet_data(data)
+	print("LOBBY ALREADY IN DICT: " + str(new_lobby_data.SERVER_IP in available_lobbys))
+	if new_lobby_data.SERVER_IP in available_lobbys:
+		available_lobbys[new_lobby_data.SERVER_IP].update_lobby_data(new_lobby_data)
+	else:
+		#instantiate label and give it packet values using MultiplayerManager.get_dict_from_packet
+		var new_label: LobbyLabel = load(lobby_label_scene).instantiate()
+		lobby_label_container.add_child(new_label)
+		available_lobbys[new_lobby_data.SERVER_IP] = new_label
+		available_lobbys[new_lobby_data.SERVER_IP].update_lobby_data(new_lobby_data)
+		
 	
-func update_lobby_members(lobby_label: LobbyLabel):
-	PacketPeer
+func update_lobby_members(lobby_ip: String):
+	available_lobbys[lobby_ip]
 	pass
