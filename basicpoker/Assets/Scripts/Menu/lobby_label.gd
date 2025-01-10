d20d3706992c8e6c3b18eeafa466653c341f81e5
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-10 17:12:59 -0300

    feat(lobby_label): updating lobby_data and gfx

--- basicpoker/Assets/Scripts/Menu/lobby_label.gd
+++ basicpoker/Assets/Scripts/Menu/lobby_label.gd
@@ -5,3 +5,14 @@ class_name LobbyLabel
 @export var lobby_name_label: Label
 @export var connected_people_label: Label
 @export var join_button: Button
+
+var lobby_data: LobbyData
+
+func update_lobby_data(new_lobby_data: LobbyData):
+	lobby_data = new_lobby_data
+	update_gfx()
+
+func update_gfx():
+	lobby_name_label.text = lobby_data.lobby_name
+	connected_people_label.text = str(lobby_data.connected_players)
+	print("Label inscripting data " + lobby_data.lobby_name)
