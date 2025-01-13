89bc57a359c32f7f052733df417c31ef8a9a3d5f
Laegha <franciscolarghero@gmail.com>
2025-01-12 23:39:15 -0300

    refactor(lobby_data): removing unfilter function to use replace_substr

--- basicpoker/Assets/Scripts/OnlineManagement/lobby_data.gd
+++ basicpoker/Assets/Scripts/OnlineManagement/lobby_data.gd
@@ -20,14 +20,10 @@ func get_packet_data() -> Dictionary:
 		"PLAYERS" : connected_players,
 		"GAMEMODE" : game_mode
 	}
+
 func set_packet_data(data: Dictionary):
 	SERVER_IP = data["IP"]
 	SERVER_PORT = data["PORT"] as int
 	filtered_lobby_name = data["LOBBY_NAME"]
 	connected_players = data["PLAYERS"] as int
 	game_mode = data["GAMEMODE"]
-
-static func unfilter_lobby_name(filtered_lobby_name: String):
-	var filters_indexes: Array[int] = GameManager.find_all_occurrences(filtered_lobby_name, name_filter)
-	for index: int in filters_indexes:
-		var filter_end = index + name_filter.length()
