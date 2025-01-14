7a364a47da8390a394b9b7bb27d1c5c3ed975a7e
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-14 14:12:55 -0300

    fix(joining_lobby): available_lobbys key composed by IP and PORT instead of IP only
    This way, multiple lobbys can be hosted by the same IP

--- basicpoker/Assets/Scripts/Menu/joining_lobby.gd
+++ basicpoker/Assets/Scripts/Menu/joining_lobby.gd
@@ -12,29 +12,22 @@ func _process(delta: float) -> void:
 	if packet_count <= 0:
 		print("ERROR RECIEVING PACKET, AVAILABLE PACKETS ARE " + str(packet_count))
 		return
+	
 	print("AVAILABLE PACKETS: " + str(packet_count))
 	var recieved_packet: String = MultiplayerManager.udp_socket.get_packet().get_string_from_utf8()
 	print("PACKET RECIEVED: " + recieved_packet)
 	on_packet_recieved(recieved_packet)
 
-func _on_return_btn_pressed() -> void:
-	GameManager.scene_manager.load_gui_scene(main_menu_scene)
-
 func on_packet_recieved(packet_data: String):
 	var data: Dictionary = MultiplayerManager.get_dict_from_packet(packet_data, LobbyData.packet_keys)
 	var new_lobby_data: LobbyData = LobbyData.new()
 	new_lobby_data.set_packet_data(data)
-	print("LOBBY ALREADY IN DICT: " + str(new_lobby_data.SERVER_IP in available_lobbys))
-	if new_lobby_data.SERVER_IP in available_lobbys:
-		available_lobbys[new_lobby_data.SERVER_IP].update_lobby_data(new_lobby_data)
+	var lobby_key: String = new_lobby_data.SERVER_IP + ":" + str(new_lobby_data.SERVER_PORT)
+	if lobby_key in available_lobbys:
+		available_lobbys[lobby_key].update_lobby_data(new_lobby_data)
 	else:
 		#instantiate label and give it packet values using MultiplayerManager.get_dict_from_packet
 		var new_label: LobbyLabel = load(lobby_label_scene).instantiate()
 		lobby_label_container.add_child(new_label)
-		available_lobbys[new_lobby_data.SERVER_IP] = new_label
-		available_lobbys[new_lobby_data.SERVER_IP].update_lobby_data(new_lobby_data)
-		
-	
-func update_lobby_members(lobby_ip: String):
-	available_lobbys[lobby_ip]
-	pass
+		available_lobbys[lobby_key] = new_label
+		available_lobbys[lobby_key].update_lobby_data(new_lobby_data)
