1719d1b636dcd40ed5c4f1636f4ca09f200c559e
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-02-27 15:05:21 -0300

    feat(client): return a bool for error checking

--- basicpoker/Assets/Scripts/OnlineManagement/client.gd
+++ basicpoker/Assets/Scripts/OnlineManagement/client.gd
@@ -2,18 +2,22 @@ class_name Client
 
 extends Node
 
-func start_client(SERVER_IP: String, SERVER_PORT: int) -> void:
+func start_client(SERVER_IP: String, SERVER_PORT: int) -> bool:
 	multiplayer.connected_to_server.connect(connection_succesful)
 	multiplayer.connection_failed.connect(connection_failed)
 	
 	var client_peer = ENetMultiplayerPeer.new()
-	client_peer.create_client(SERVER_IP, SERVER_PORT)
+	if(client_peer.create_client(SERVER_IP, SERVER_PORT) != OK):
+		return false
+	
+	
 	if multiplayer.multiplayer_peer:
 		multiplayer.multiplayer_peer.close()
 		
 	multiplayer.multiplayer_peer = client_peer
 	multiplayer.multiplayer_peer.peer_connected.connect(peer_connected)
 	MultiplayerManager.set_host()
+	return true
 	print("STATUS " + str(multiplayer.multiplayer_peer.get_connection_status()))
 	print("Client got host " + str(MultiplayerManager.host))
 
