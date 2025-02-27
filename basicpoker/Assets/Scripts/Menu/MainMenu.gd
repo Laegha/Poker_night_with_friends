acdf2f483a5ddc0fe70b5ec496505124f89f1b9a
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-02-27 15:05:43 -0300

    feat(MainMenu): managing joining lobby using user ip input

--- basicpoker/Assets/Scripts/Menu/MainMenu.gd
+++ basicpoker/Assets/Scripts/Menu/MainMenu.gd
@@ -2,6 +2,9 @@ extends Control
 
 @export var name_input_field: LineEdit
 @export var lobby_ip_input_field: LineEdit
+@export var invalid_ip_message: String
+@export var ip_input_animated_sprite: AnimatedSprite2D
+
 
 var create_lobby_scene: String = "res://Scenes/MainMenu/creating_lobby.tscn"
 var client_scene: String = "res://Scenes/MainMenu/joining_lobby.tscn"
@@ -16,7 +19,16 @@ func _on_create_lobby_pressed() -> void:
 
 func _on_join_lobby_pressed() -> void:
 	update_player_name()
-	var host: Client = load(client_scene).instantiate()
+	var ip: String
+	var port: int
+	var parts = lobby_ip_input_field.text.split(":")
+	if parts.size() == 2:
+		ip = parts[0]
+		port = parts[1] as int
+	else:
+		lobby_ip_input_field.text = 
+	var client: Client = load(client_scene).instantiate()
+	if(client.start_client(lobby_ip_input_field.text, MultiplayerManager))
 	GameManager.root_node.add_child(host)
 	GameManager.scene_manager.load_gui_scene(client_lobby_scene)
 
